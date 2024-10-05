ad48321909e696985ad4566b15d25ec3f66253fc
acol <panager@gmail.com>
2024-10-05 22:16:01 +0300

    stuck here

--- Ask1/test.cpp
+++ Ask1/test.cpp
@@ -1,40 +1,53 @@
 #include <fcntl.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <poll.h>
+#include <pthread.h>
+#include <sys/poll.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include <cctype>
 #include <csignal>
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <string>
 
+bool terminate{};
+
+void *KeepSecretLinkUp(void *);
 void SigAction(int signal, siginfo_t *info, void *);
+int GetNum(char *buf, int &i, int n);
 
 int main(int argc, char *argv[]) {
-    // NOTE(acol): making read only file for 0 and 1
+    // NOTE(acol): making read only file (0) (1)
     creat("./.hello_there", S_IRUSR);
 
-    // NOTE(acol): unpause with SIGCONT for 2
+    // NOTE(acol): unpause with SIGCONT (2)
     struct sigaction act{};
     act.sa_sigaction = SigAction;
     act.sa_flags = SA_RESTART | SA_SIGINFO | SA_NOCLDWAIT;
     if (sigemptyset(&act.sa_mask)) return 1;
     if (sigaction(SIGCHLD, &act, NULL)) return 1;
 
-    // NOTE(acol): environment variables for 3 and extra hints
+    // NOTE(acol): environment variables for (3) and extra hints
     setenv("ANSWER", "42", 0);
     setenv("I_NEED_TECH_HINTS", "1", 0);
 
     // NOTE(acol): fifo for 4 with rw-r--r-- perms
     mkfifo("./magic_mirror", 0644);
 
-    // NOTE(acol): creating an fd 99 for 5
+    // NOTE(acol): creating an fd 99 (5)
     int fd = open("./test.cpp", O_APPEND);
     dup2(fd, 99);
 
-    // NOTE(acol): making ping pong pipes for 6
+    // NOTE(acol): making ping pong pipes (6)
     int pingfd[2];
     pipe(pingfd);
     dup2(pingfd[0], 33);
@@ -45,51 +58,184 @@ int main(int argc, char *argv[]) {
     dup2(pongfd[0], 53);
     dup2(pongfd[1], 54);
 
-    // NOTE(acol): hard link .hello_there to .hey_there for 7
+    // NOTE(acol): hard link .hello_there to .hey_there (7)
     link("./.hello_there", "./.hey_there");
 
-    // NOTE(acol): make sparce files bf00 to bf09 with user read write perms and
-    // writing 16 chars at 1073741824 for 8
-    char buff[]{"aaaaaaaaaaaaaaa"};
+    // NOTE(acol):  make sparce files bf00 to bf09 with user read write perms
+    //              and writing 16 chars at 1073741824 for each one (8)
+    char buf1[]{"aaaaaaaaaaaaaaa"};
 
     std::string name{"bf0"};
     for (int i{}; i < 10; ++i) {
         fd = open((name + std::to_string(i)).c_str(), O_WRONLY | O_CREAT,
                   S_IRUSR | S_IWUSR);
         lseek(fd, 1073741824, SEEK_SET);
-        write(fd, buff, 16);
+        write(fd, buf1, 16);
+        close(fd);
     }
 
+    // NOTE(acol):  making an tcp server side for (9)
+    int sd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    // NOTE(acol):  have to use same port like ssh, how do you dodge that on the
+    //              internet ??
+    sin.sin_port = htons(49842);
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sd, (struct sockaddr *)&sin, sizeof(sin)) < 0) return 1;
+
+    listen(sd, 1);
+
+    // NOTE(acol): (10) and (11)
+    creat("./secret", S_IRUSR | S_IWUSR);
+    link("secret", "secret_number");
+
+    int child_sdin_pipe[2];
+    pipe(child_sdin_pipe);
     pid_t cpid = fork();
     if (cpid < 0) return 1;
 
     if (!cpid) {
+        // NOTE(acol): making this pipe the stdin for child
+        dup2(child_sdin_pipe[0], STDIN_FILENO);
+        close(child_sdin_pipe[1]);
         execl("./riddle", "./riddle");
     } else {
-        // NOTE(acol): CHILD PROROCESS DOESNT RAISE SIGCHLD WHEN IT PAUSES
-        //             so I had to do this scuffed shit WHY????
-        usleep(600);
+        close(child_sdin_pipe[0]);
+        // FIX(acol): CHILD PROROCESS DOESNT RAISE SIGCHLD WHEN IT PAUSES
+        //            so I had to do this scuffed shit for (2)
+        //            why????
+        sleep(1);
         kill(cpid, SIGCONT);
 
-        for (;;) {
-            usleep(100);
-        }
-    }
+        // NOTE(acol): putting accept from (9) here cause it's blocking
+        struct sockaddr_in peer_addr;
+        socklen_t peer_addr_len;
+        int num1{}, num2{}, i{};
+        char op{};
+        char buf[100];
 
-    for (int i{}; i < 2; ++i) {
-        close(pingfd[i]);
-        close(pongfd[i]);
+        int sockfd = accept(sd, (struct sockaddr *)&peer_addr, &peer_addr_len);
+        int n = read(sockfd, buf, 100);
+        for (; i < n && !isdigit(buf[i]); ++i) {
+        }  // find first num
+
+        for (; i < n; ++i) {
+            if (buf[i] == '+' || buf[i] == '-' || buf[i] == '/' ||
+                buf[i] == '*') {
+                op = buf[i];
+            } else if (isdigit(buf[i])) {
+                if (num1 == 0) {
+                    num1 = GetNum(buf, i, n);
+                } else {
+                    num2 = GetNum(buf, i, n);
+                    switch (op) {
+                        case '+': {
+                            num1 += num2;
+                        } break;
+                        case '-': {
+                            num1 -= num2;
+                        } break;
+                        case '*': {
+                            num1 *= num2;
+                        } break;
+                        case '/': {
+                            num1 /= num2;
+                        } break;
+                    }
+                }
+            }
+        }
+        write(sockfd, std::to_string(num1).c_str(),
+              std::to_string(num1).length() + 1);
+
+        pthread_t thread1;
+        pthread_create(&thread1, nullptr, KeepSecretLinkUp, nullptr);
+
+        sleep(1);
+        memset(buf, 0, sizeof(buf));
+        int secret_fd = open("./secret", O_RDONLY);
+        std::string secret_answer;
+        i = 0;
+        read(secret_fd, buf, sizeof(buf));
+        for (; i < 100 && buf[i] != ':'; ++i) {
+        }
+        i += 2;
+        while (i < 100 && buf[i] != '\n') {
+            secret_answer += buf[i++];
+        }
+        secret_answer += '\n';
+        std::cout << secret_answer;
+        write(child_sdin_pipe[1], secret_answer.c_str(),
+              (int)(secret_answer.size() + 1));
+
+        sleep(1);
+        memset(buf, 0, sizeof(buf));
+        secret_answer.clear();
+        i = 0;
+        read(secret_fd, buf, sizeof(buf));
+        for (; i < 100 && buf[i] != ':'; ++i) {
+        }
+        i += 2;
+        while (i < 100 && buf[i] != '\n') {
+            secret_answer += buf[i++];
+        }
+        secret_answer += '\n';
+        std::cout << secret_answer;
+        write(child_sdin_pipe[1], secret_answer.c_str(),
+              (int)(secret_answer.size() + 1));
+        close(secret_fd);
+        pthread_join(thread1, nullptr);
+
+        while (!terminate) {
+            usleep(10000);
+        }
+
+        for (int i{}; i < 2; ++i) {
+            close(pingfd[i]);
+            close(pongfd[i]);
+        }
+        close(sd);
+        close(sockfd);
+
+        name = "bf0";
+        for (int i{}; i < 10; ++i) {
+            unlink((name + std::to_string(i)).c_str());
+        }
+        unlink("./magic_mirror");
+        unlink("./riddle.savegame");
+        unlink("./.hey_there");
+        unlink("./.hello_there");
+        unlink("./secret");
     }
     return 0;
 }
 
 void SigAction(int signal, siginfo_t *info, void *) {
-    if (info->si_code == CLD_STOPPED || info->si_code == CLD_TRAPPED) {
+    if (info->si_code == CLD_STOPPED) {
         std::cout << "child paused\n";
         if (kill(info->si_pid, SIGCONT)) raise(SIGTERM);
     } else if (info->si_code == CLD_CONTINUED) {
         std::cout << "resumed\n";
     } else if (info->si_code == CLD_EXITED) {
-        raise(SIGTERM);
+        terminate = true;
     }
+}
+
+int GetNum(char *buf, int &i, int n) {
+    int answer{};
+    for (; i < n && isdigit(buf[i]); ++i) {
+        answer = answer * 10 + buf[i] - '0';
+    }
+    return answer;
+}
+
+void *KeepSecretLinkUp(void *) {
+    struct stat buff;
+    while (!lstat("./secret_number", &buff)) {
+        usleep(10);
+    }
+    link("./secret", "./secret_number");
+    return nullptr;
 }
